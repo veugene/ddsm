186516ede1e1bfc8dc2e0abec995f20aa2bbfa8d
Eugene Vorontsov <eugene.vorontsov@gmail.com>
2021-06-02 15:37:24 -0400

    Reconstruct only mass masks (no calcifications) and blacklist cases with missing mass ROIs.

--- data_scripts/prepare_inbreast.py
+++ data_scripts/prepare_inbreast.py
@@ -15,6 +15,11 @@ from skimage.morphology import (binary_closing,
 from tqdm import tqdm
 
 
+# Missing mass ROI.
+BAD_CASES = ['22614097',
+             '22614150']
+
+
 def get_parser():
     parser = argparse.ArgumentParser(description=""
         "Create an HDF5 dataset with INbreast data. Sick images are those "
@@ -35,11 +40,11 @@ def get_parser():
 
 def load_inbreast_mask(mask_path, imshape=(4084, 3328)):
     """
-    Shameless copy from:
+    Copied (and slightly modified) from:
     https://gist.github.com/Feyn-Man/de6f62997d051fc6ff75a6aa968537f5
     
-    This function loads a osirix xml region as a binary numpy array for INBREAST
-    dataset
+    This function loads an osirix xml region as a binary numpy array for
+    the INBREAST dataset
     @mask_path : Path to the xml file
     @imshape : The shape of the image as an array e.g. [4084, 3328]
     return: numpy array where positions in the roi are assigned a value of 1.
@@ -47,23 +52,26 @@ def load_inbreast_mask(mask_path, imshape=(4084, 3328)):
 
     mask = np.zeros(imshape)
     with open(mask_path, 'rb') as mask_file:
-        plist_dict = plistlib.load(mask_file, fmt=plistlib.FMT_XML)['Images'][0]
+        plist_dict = plistlib.load(mask_file,
+                                   fmt=plistlib.FMT_XML)['Images'][0]
         numRois = plist_dict['NumberOfROIs']
         rois = plist_dict['ROIs']
         assert len(rois) == numRois
         for roi in rois:
-            numPoints = roi['NumberOfPoints']
-            points = roi['Point_px']
-            assert numPoints == len(points)
-            points = [eval(point) for point in points]
-            if len(points) <= 2:
-                for point in points:
-                    mask[int(point[1]), int(point[0])] = 1
-            else:
-                x, y = zip(*points)
-                col, row = np.array(x), np.array(y) ##x coord is the column coord in an image and y is the row
-                poly_x, poly_y = polygon(row, col, shape=imshape)
-                mask[poly_x, poly_y] = 1
+            if roi['Name']=='Mass':
+                numPoints = roi['NumberOfPoints']
+                points = roi['Point_px']
+                assert numPoints == len(points)
+                points = [eval(point) for point in points]
+                if len(points) <= 2:
+                    for point in points:
+                        mask[int(point[1]), int(point[0])] = 1
+                else:
+                    x, y = zip(*points)
+                    ##x coord is the column coord in an image and y is the row
+                    col, row = np.array(x), np.array(y)
+                    poly_x, poly_y = polygon(row, col, shape=imshape)
+                    mask[poly_x, poly_y] = 1
     return mask.astype(np.uint8)
 
 
@@ -90,6 +98,8 @@ def prepare_data_inbreast(args):
             "^([0-9]{8})_([a-z0-9]{16})_MG_(L|R)_(CC|ML|FB)_ANON.dcm$", fn)
         assert match is not None
         case, patient, laterality, view = match.groups()
+        if case in BAD_CASES:
+            continue
         if case in cases_with_mass:
             key = 's'
             assert os.path.exists(os.path.join(args.path_inbreast,
